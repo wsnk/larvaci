54f38f747baeabc7a031b87c7df0f8fcaab36a51
wsnk <wsnk00@yandex.com>
2020-06-11 13:41:12 +0300

    Some more features for git & command

--- larvaci/command.py
+++ larvaci/command.py
@@ -7,21 +7,25 @@ import time
 class Command:
     def __init__(self, logger=None):
         self.logger = logger or logging.getLogger()
+        self.stdout = bytearray()
         self._tasks = []
 
     def _read_output(self, reader):
         async def read():
             while True:
-                l = (await reader.readline()).decode("utf-8").strip()
+                l = await reader.readline()
+                self.stdout += l
                 if not l:
                     self.logger.debug(f"OUT: <EOF>")
                     return
-                self.logger.debug(f"OUT: {l}")
+                s = l.decode("utf-8").strip()
+                self.logger.debug(f"OUT: {s}")
 
         self._tasks.append(asyncio.create_task(read()))
 
     async def run(self, args, cwd=None):
         self.logger.debug(f"Run command: {args}...")
+        self.stdout = bytearray()
 
         self.proc = await asyncio.create_subprocess_exec(
             *args,
@@ -37,3 +41,9 @@ class Command:
 
     async def wait(self):
         return await self.proc.wait()
+
+    async def exec(self, *args, **kwargs):
+        await self.run(*args, **kwargs)
+        retcode = await self.wait()
+        if retcode != 0:
+            raise RuntimeError(f"command failed with code = {retcode}")
--- larvaci/git.py
+++ larvaci/git.py
@@ -14,13 +14,19 @@ class Git:
             raise RuntimeError(f"git command failed with code = {retcode}")
 
     async def clone(self, url, dst_dir, branch=None):
-        opts = ["--recurse-submodules", "--shallow-submodules"]
-        if branch is not None:
-            opts += ["--branch", branch]
-        return await self.run(["clone"] + opts + [url, dst_dir])
+        return await self.run(["clone", url, dst_dir])
 
     async def clean(self, repo_dir):
         return await self.run(["clean", "--force", "-d", "-x"], cwd=repo_dir)
 
     async def checkout(self, repo_dir, revision):
-        return await self.run(["checkout", "--force", "--recurse-submodules", revision], cwd=repo_dir)
+        return await self.run(["checkout", "--force", revision], cwd=repo_dir)
+
+    async def fetch(self, repo_dir):
+        return await self.run(["fetch", "--force"], cwd=repo_dir)
+
+    async def submodule_init(self, repo_dir):
+        return await self.run(["submodule", "update", "--init", "--recursive"], cwd=repo_dir)
+
+    async def submodule_deinit(self, repo_dir):
+        return await self.run(["submodule", "deinit", "--force", "--all"], cwd=repo_dir)
